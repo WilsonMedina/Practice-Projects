ad77722da7859bbd8e74589c42ac6c6d1d370329
wilsonmedina <pemdastau@gmail.com>
2023-02-18 22:26:13 -0300

    add changes

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -6,5 +6,7 @@ path_web = input('Write your name of domain [Example= example.com]: ')
 path_follow = 'https://' + path_web
 filename = route + '.png'
 
+
+
 image = qrcode.make(path_follow)
 image.save(filename)
